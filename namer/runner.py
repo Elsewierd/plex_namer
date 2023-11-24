a36a67d7a63133d49629ce925a03bdc7cbfa8c59
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-24 14:56:23 -0500

    wrote preference loading and saving functions

--- namer/runner.py
+++ namer/runner.py
@@ -1,3 +1,4 @@
+import json
 import cv2
 
 
@@ -23,6 +24,63 @@ def plex_compatable(file: str) -> bool:
         # Release the video capture object, if it was opened
         video.release()
 
+def preferences_load(filepath: str, *args) -> str:
+    """Loads prefernces from file, if there are no *args returns the whole file as a string
 
+    Args:
+        filepath (str): Filepath to JSON file to be loaded from.
+        *args (tuple): Keys needed to navigate to the value(s)
+
+    Raises:
+        KeyError: The keys do not exisit in the called order.
+        e: Exception caught if filepath does not open properly.
+
+    Returns:
+        str: The portion of the JSON file dictated by the keys (args).
+    """    
+    try:
+        with open(filepath, "r") as file_json:
+            data = json.load(file_json)
+            for arg in args:
+                if data.get(arg) is None:
+                    raise KeyError(f"Error in the preference keys: {args}")
+                data = data[arg]
+            
+            return str(data)
+    except Exception as e:
+        raise e
+
+def preference_save(filepath:str, value:Any=None, create:bool=False, *args) -> None:
+    """Saves a prefence value to file
+
+    Args:
+        filepath (str): Filepath to JSON file to be altered.
+        value (Any, optional): Value to be saved. Defaults to None.
+        create (bool, optional): Boolean flag denoting weather to create . Defaults to False.
+        *args (tuple): Keys needed to navigate to the value
+
+    Raises:
+        KeyError: If create is not True and the keys do not exisit in the called order.
+        e: Exception caught if filepath does not open properly.
+    """    
+    try:
+        with open(filepath, "r") as file_json:
+            data = json.load(file_json)
+        
+        current_level = data
+        for i, arg in enumerate(args):
+            if not create and current_level.get(arg) is None:
+                raise KeyError(f"Error in the preference keys: {args}")
+            elif i == len(args) - 1:
+                # If it's the last key, set the value
+                current_level[arg] = value
+            else:
+                # If it's not the last key, update the current level
+                current_level = current_level.setdefault(arg, {})
+        
+        with open(filepath, "w") as file_json:
+            json.dump(data, file_json, indent=2)
+    except Exception as e:
+        raise e
 if __name__ == "__main__":
-    
+...
